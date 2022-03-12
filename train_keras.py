ed6d68d83fc36f88944d3286d1d576c43707bf85
Pengcheng Xu <xu_pengcheng@sjtu.edu.cn>
2022-03-13 01:03:27 +0800

    delete VAE

--- train_keras.py
+++ train_keras.py
@@ -68,7 +68,7 @@ def run(path,date,code, X_train, y_train, platform, model_type, data_type,HIDDEN
     data_dict = {'origin_data': origin_data, 'square_data': square_data, 'log_data': log_data,
                  'radical_data': radical_data, 'cube_data': cube_data}
     model_dict = {'LinearRegression': LinearRegression, 'LogisticRegression': LogisticRegression,
-                  'L1': Lasso, 'L2': Ridge, 'RandomForest': RandomForestRegressor,'VAE':VAE.VAE,'AE':AE.Autoencoder}
+                  'L1': Lasso, 'L2': Ridge, 'RandomForest': RandomForestRegressor,'AE':AE.Autoencoder}
 
     with open(platform, 'r') as f:
         gene_dict = json.load(f)
